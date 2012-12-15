d015386b0b45ec8a6dee5bb6984508d90450e4b0
drcable <drcable@tarbwf.com>
2012-12-15 19:26:29 +0000

    now runs via comand line options, has better documentation, and sensible filnames

--- proceduralgenerator.py
+++ proceduralgenerator.py
@@ -6,31 +6,56 @@ from collections import Counter
 import pickle as p
 from random import random, choice
 import math
-def makehist():
-    textin = open('bibleraw.t', 'U').read()
+import sys
+def makehist(filename):
+    textin = open(filename, 'U').read()
     words = textin.replace('\n'," ").split(' ')
     histogram = {}
     for baseword in list(set(words)):
-        followingfreqs = {}#a dict of (wount, score) turples for each word(key)
+        followingfreqs = {}#a dict of [wount, score] lists for each word(key)
         for pos in [i for i, x in enumerate(words) if x == baseword]:
             try:
                 followingword = words[pos+1]
                 try:
                     followingfreqs[followingword] = [followingfreqs[followingword][0] + 1,followingfreqs[followingword][1]]
                 except KeyError:
-                    followingfreqs[followingword] = [1,0]#set (count,score) turple to 1 count, no score
+                    followingfreqs[followingword] = [1,0]
             except:
                 pass
         histogram[baseword] = followingfreqs
     p.dump(histogram,open("histogramout.pickle","w"))
-def calcwords():
+def calcwords(length):
     histogram = p.load(open("histogramout.pickle","r"))
     word = choice(histogram.keys())
     sentance = word
-    while len(sentance) <130:
+    while len(sentance) <length-10:
         wordscores = [[key,histogram[word][key][0]] for key in histogram[word]]
         wordscores.sort(key=lambda x: x[1])
-        word = wordscores[int(math.floor((random()**8)*len(wordscores)))][0]
+        word = wordscores[(int(math.floor((random()**2)*len(wordscores))))][0]
         sentance = sentance + " " + word
     print sentance
-calcwords()
+helptext = """
+Options:
+-m=<filename> Produces a histogram for the generation of text from filename.
+-p=<charecters> Prints <charecters> of text generated from the above histogram
+-h Print this help text.
+"""
+for argument in sys.argv[1:]:
+    if argument[:2] == "-m":
+        if argument[3:] != "":
+            makehist(argument[3:])
+        else:
+            print "Make histogram error - no source file specified"
+            print helptext
+    elif argument[:2] == "-p":
+        try:
+            calcwords(int(argument[3:]))
+        except:
+            print "you need to specify a charecter limit"
+            print helptext
+    elif argument[:2] == "-h":
+        print helptext
+    else:
+        print "error unrecognized argument:" + argument
+        print helptext
+    
